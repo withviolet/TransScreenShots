9978047040dfbaf9158e21b6be1a6adcf15cecda
chenxiaohan <chenxiaohan@tenorshare.com>
2022-01-06 17:18:43 +0800

    创建新工程

---
+++ TransScreenShots/TransScreenShots/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TransScreenShots
+//
+//  Created by Chen Xiaohan on 2022/1/6.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
