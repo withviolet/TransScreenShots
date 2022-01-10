d4268e354b44381b9fcdd58a8555f5d589b65f11
withviolet <1071069722@qq.com>
2022-01-10 22:08:14 +0800

    修改

---
+++ MyTodo/MyTodo/TodoDetails.swift
@@ -0,0 +1,20 @@
+//
+//  TodoDetails.swift
+//  MyTodo
+//
+//  Created by WithViolet on 2022/1/10.
+//
+
+import SwiftUI
+
+struct TodoDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoDetails()
+    }
+}
